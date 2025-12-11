5b7a20cfb81a5e9ae30aa3ed80fa9e374b4fc123
joel <joelsantossouza08@gmail.com>
2025-12-11 14:56:02 +0000

    Fix the cmp instruction, specifing the size of dereference of [rdi + rax] (dereference to one byte, to be a char)

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,21 +1,9 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/09 11:37:26 by joesanto          #+#    #+#              #
-#    Updated: 2025/12/11 14:44:47 by joesanto         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 global	ft_strlen
 ft_strlen:
 	xor	rax, rax
 
 iterate_str:
-	cmp	[rdi + rax], 0
+	cmp	byte [rdi + rax], 0
 	je	exit
 	add	rax, 1
 	ret
